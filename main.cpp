2561d74de6f998893c4e7cce192f1d3e2152d0d1
Simiyutin Boris <simiyutin.boris@yandex.ru>
2018-02-26 01:22:46 +0300

    task done

---
+++ main.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <fstream>
+#include <bitset>
+#include <netinet/in.h>
+#include <vector>
+#include <unordered_set>
+
+using ui = uint32_t;
+
+union platter {
+    struct {
+        unsigned C:3, B:3, A:3, :23;
+    } std;
+
+    struct {
+        unsigned value:25, A:3, :4;
+    } spc;
+
+    struct {
+        unsigned :28, val:4;
+    } op;
+
+    ui value;
+    char bytes[4];
+};
+
+ui registers[8] = {0, 0, 0, 0, 0, 0, 0 ,0};
+
+std::vector<std::vector<platter>> arrays;
+std::unordered_set<ui> freedArrays;
+
+int main(int argc, char ** argv) {
+
+    std::ifstream is(argv[1], std::ios::in | std::ios::binary);
+    std::vector<platter> zeroArray;
+
+    while(is.peek() != EOF) {
+        union platter p{};
+        is.read(p.bytes, 4);
+        p.value = ntohl(p.value);
+        zeroArray.push_back(p);
+    }
+    arrays.push_back(zeroArray);
+
+    size_t executionPoint = 0;
+    while (executionPoint < arrays[0].size()) {
+        union platter p = arrays[0][executionPoint++];
+
+        ui A      = p.std.A;
+        ui B      = p.std.B;
+        ui C      = p.std.C;
+        ui OP     = p.op.val;
+        ui SPCA   = p.spc.A;
+        ui SPCVAL = p.spc.value;
+
+        switch (OP) {
+            case 0:
+                if (registers[C] != 0) {
+                    registers[A] = registers[B];
+                }
+                break;
+            case 1:
+                registers[A] = arrays[registers[B]][registers[C]].value;
+                break;
+            case 2:
+                arrays[registers[A]][registers[B]].value = registers[C];
+                break;
+            case 3:
+                registers[A] = registers[B] + registers[C];
+                break;
+            case 4:
+                registers[A] = registers[B] * registers[C];
+                break;
+            case 5:
+                registers[A] = registers[B] / registers[C];
+                break;
+            case 6:
+                registers[A] = ~(registers[B] & registers[C]);
+                break;
+            case 7:
+                exit(0);
+            case 8: {
+                ui id;
+                if (freedArrays.empty()) {
+                    std::vector<platter> newArray(registers[C]);
+                    arrays.push_back(newArray);
+                    id = static_cast<ui>(arrays.size() - 1);
+                } else {
+                    ui freeArray = *freedArrays.begin();
+                    freedArrays.erase(freeArray);
+                    arrays[freeArray].resize(registers[C]);
+                    id = freeArray;
+                }
+                registers[B] = id;
+                break;
+            }
+            case 9: {
+                ui id = registers[C];
+                arrays[id].clear();
+                freedArrays.insert(id);
+                break;
+            }
+            case 10:
+                std::cout << (unsigned char) registers[C] << std::flush;
+                break;
+            case 11: {
+                unsigned char c;
+                if (std::cin >> c) {
+                    registers[C] = c;
+                } else {
+                    registers[C] = 0xffffffff;
+                }
+                break;
+            }
+            case 12:
+                arrays[0] = arrays[registers[B]];
+                executionPoint = registers[C];
+                break;
+            case 13:
+                registers[SPCA] = SPCVAL;
+                break;
+            default:
+                exit(1);
+        }
+
+    }
+
+    return 0;
+}
